323e3a93d7011b04bdfecac5b629d4e040f46ae0
Raymond Boswel <raymondboswel@gmail.com>
2021-09-10 15:08:24 +0200

    Added fn to filter duplicates using hash map

--- src/ch1/ex1.rs
+++ src/ch1/ex1.rs
@@ -2,6 +2,11 @@ use crate::ch1::read_lines::read_lines;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::collections::VecDeque;
+
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+use std::collections::HashMap;
 
 pub fn reverse_lines<P>(path: P)
 where
@@ -43,5 +48,67 @@ where
         while let Some(top) = stack.pop() {
             println!("{}", top)
         }
+    }
+}
+
+pub fn queue_n_till_blank_line<P>(path: P, n: u8) where P: AsRef<Path> {
+
+    let max_length = 5;
+    let mut queue = VecDeque::with_capacity(6);
+    if let Ok(lines) = read_lines(path) {
+        for res in lines {
+            if let Ok(line) = res {
+                queue.push_front(line);
+                if queue.len() > max_length {
+                    queue.pop_back();
+                }
+                let empty = "".to_string();
+                match queue.front() {
+                    Some(l) => {
+                        match l.as_ref() {
+                            "" => println!("{}",queue.back().unwrap()),
+                            _ => println!(""),
+                        }
+                    }
+                    _ => {
+                        println!("Nothing at front of queue!")
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+pub fn filter_duplicates<P>(path: P) where P: AsRef<Path> {
+
+    let mut uniq_lines =  HashMap::new();
+
+    if let Ok(lines) = read_lines(path) {
+        for res in lines {
+            if let Ok(line) = res {
+                // Create hash of string
+                let mut s = DefaultHasher::new();
+                line.hash(&mut s);
+                let hash = s.finish();
+                if !uniq_lines.contains_key(&hash) {
+                    uniq_lines.insert(hash, line);
+
+                    println!("{}", uniq_lines.get(&hash).unwrap());
+                }
+
+
+                // stack.push(line);
+                // if stack.len() == n.into() {
+                //     while let Some(top) = stack.pop() {
+                //         println!("{}", top)
+                //     }
+                // }
+            }
+        }
+
+        // while let Some(top) = stack.pop() {
+        //     println!("{}", top)
+        // }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,15 @@ mod ch1 {
 
 use ch1::ex1::reverse_lines;
 use ch1::ex1::reverse_n_lines;
+use ch1::ex1::queue_n_till_blank_line;
+use ch1::ex1::filter_duplicates;
 
 fn main() {
     // reverse_lines("./poem.txt");
 
-    reverse_n_lines("./poem.txt", 3);
+    // reverse_n_lines("./poem.txt", 3);
+
+    // queue_n_till_blank_line("./poem.txt", 3);
+
+    filter_duplicates("./poem.txt");
 }
